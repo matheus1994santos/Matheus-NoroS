650c189e39a608d3276fd87a3593cafc9d4fdbc5
Tél Noronhes <73230985+matheus1994santos@users.noreply.github.com>
2020-11-06 19:36:52 -0300

    LPL Ciência
    Primeiro Projeto.

---
+++ LPL Ciencia/home-page/js/function.js
@@ -0,0 +1,26 @@
+$(function(){
+    //$(header).css({"background-color": "yellow", "font-size": "200%"});
+    //$('header').css('background-image', 'linear-gradient(to bottom right, #191387,#3e37bf,#463ede,#1c167d)');
+    $('.container').css();
+    $('#logo1').css('');
+    $('#logo2').css('');
+    $('#titulo').css('');
+    $('.menu-pai').css('');
+    $('').css('');
+    $('ul').css('');
+    $('li').css('');
+    
+   
+});
+
+$(document).ready(function(){
+  $("#b1").click(function(){
+    $(".sub-menu").slideToggle();
+  });
+});
+
+
+
+
+
+
